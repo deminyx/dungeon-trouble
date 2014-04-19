045f80f77ffec78f978d8e01c6eb7a21b7ebb07e
Maxime BELLIER <maxime.bellier@etu.univ-nantes.fr>
2014-04-19 14:20:21 +0200

    Première Stucture du Module Partie (Partie>Niveau>Objets>...)

--- src/fr/dungeontrouble/partie/Partie.java
+++ src/fr/dungeontrouble/partie/Partie.java
@@ -1,6 +1,13 @@
 package fr.dungeontrouble.partie;
 
+import fr.dungeontrouble.partie.niveau.Niveau;
+/**
+ * Module Partie
+ * Classe principale décrivant l'ensemble des paramètres et méthodes d'une partie de Dungeon Trouble
+ * @author Maxime BELLIER
+ *
+ */
 public class Partie {
 
-	
+	Niveau niv;
 }
