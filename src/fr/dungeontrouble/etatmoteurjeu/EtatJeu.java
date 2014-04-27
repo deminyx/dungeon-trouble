939d2ac24421b31ec29063e2593c6d41994a7175
Maxime BELLIER <maxime.bellier@etu.univ-nantes.fr>
2014-04-27 15:43:39 +0200

    Ajout Classe Etat du Jeu

---
+++ src/fr/dungeontrouble/etatmoteurjeu/EtatJeu.java
@@ -0,0 +1,15 @@
+package fr.dungeontrouble.etatmoteurjeu;
+
+import org.jsfml.system.Clock;
+
+import fr.dungeontrouble.affichage.Affichage;
+import fr.dungeontrouble.evenement.Evenement;
+import fr.dungeontrouble.partie.Partie;
+
+public class EtatJeu {
+
+	private Clock clock;
+	private Partie partie;
+	private Affichage affichage;
+	private Evenement event;
+}
