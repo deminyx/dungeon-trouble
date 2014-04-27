be71e53a3275cfdfd6af7aff7a52da6d558b149e
TeKrop <tekrop@gmail.com>
2014-04-27 22:59:10 +0200

    Correction de la mise en place des portes dans le tableau d'objets ==> It works \o/

--- src/fr/dungeontrouble/partie/niveau/Niveau.java
+++ src/fr/dungeontrouble/partie/niveau/Niveau.java
@@ -71,7 +71,6 @@ public class Niveau {
 				temp = scannerLine.nextLine(); // Récupération de la ligne
 				scannerTemp = new Scanner(temp); // Assignation au scanner
 				scannerTemp.useDelimiter(","); // Délimitation : virgules
-				boolean nouvellePorte = true;
 				
 				// Tant qu'il reste des entiers sur la ligne
 				while (scannerTemp.hasNext()) {
@@ -85,22 +84,7 @@ public class Niveau {
 						Objet obj;
 						switch(champ){
 							case 11:
-//								Vector2f pos = new Vector2f(compteur / NBCOLONNES, compteur % NBCOLONNES);
-//								// On a une nouvelle porte si il n'y a pas déjà de porte en haut ou à gauche
-//								nouvellePorte =(!(
-//									( // Si ce n'est pas en haut et qu'il y a une porte au dessus
-//										(pos.y != 0)&&
-//										(objets.containsKey(new Vector2f(pos.x, pos.y-1))&&
-//										(objets.get(new Vector2f(pos.x, pos.y-1)) instanceof Porte))
-//									)
-//								||
-//									( // Si ce n'est pas à gauche et qu'il y a une porte à gauche
-//										(pos.x != 0)&&
-//										(objets.containsKey(new Vector2f(pos.x-1, pos.y))&&
-//										(objets.get(new Vector2f(pos.x-1, pos.y-1)) instanceof Porte))
-//									)
-//								));
-								obj = new Porte(TypePorte.MorceauPorteHorizontal, nouvellePorte); 
+								obj = assignPorte(compteur, objets, TypePorte.MorceauPorteHorizontal);								
 								break;
 							case 12:
 								obj = new Generateur(TypeGenerateur.fantome);								
@@ -109,19 +93,19 @@ public class Niveau {
 								obj = new Generateur(TypeGenerateur.gobelin);
 								break;
 							case 20:
-								obj = new Porte(TypePorte.BoutPorteDroite, nouvellePorte);
+								obj = assignPorte(compteur, objets, TypePorte.BoutPorteDroite);	
 								break;
 							case 21:
-								obj = new Porte(TypePorte.BoutPorteGauche, nouvellePorte);
+								obj = assignPorte(compteur, objets, TypePorte.BoutPorteGauche);
 								break;
 							case 22:
-								obj = new Porte(TypePorte.BoutPorteBas, nouvellePorte);
+								obj = assignPorte(compteur, objets, TypePorte.BoutPorteBas);
 								break;
 							case 23:
-								obj = new Porte(TypePorte.BoutPorteHaut, nouvellePorte);
+								obj = assignPorte(compteur, objets,	TypePorte.BoutPorteHaut);
 								break;
 							case 24:
-								obj = new Porte(TypePorte.MorceauPorteVertical, nouvellePorte);
+								obj = assignPorte(compteur, objets,	TypePorte.MorceauPorteVertical);
 								break;
 							case 26:
 								obj = new Tresor();
@@ -160,6 +144,59 @@ public class Niveau {
 			scannerTemp.close();
 		} catch (Exception e) {
 			e.printStackTrace(); // On catch l'exception en cas de problème
+		}
+	}
+	
+	/**
+	 * Méthode de vérification de présence de bout de porte sur une case au dessus ou à gauche
+	 * @param nouvellePorte Booléan que l'on met à true si on n'a pas trouvé de bout de porte
+	 * @param idPorte Id de porte à laquelle on affecte l'id d'un bout de porte si trouvé
+	 * @param compteur Compteur de cases dans l'algorithme de lecture de fichier contenant la carte
+	 * @param objets HashMap des objets déjà trouvés sur la map
+	 */
+	public static int verifPorte(int compteur, HashMap<Vector2f,Objet> objets){		
+		Vector2f pos = new Vector2f(compteur / NBCOLONNES, compteur % NBCOLONNES);
+		Integer idPorte = -1;
+		
+		// On a une nouvelle porte si il n'y a pas déjà de porte en haut ou à gauche
+		if ( // Si ce on n'est pas en haut et qu'il y a une porte au dessus
+				(pos.y != 0)&&
+				(objets.containsKey(new Vector2f(pos.x, pos.y-1))&&
+				(objets.get(new Vector2f(pos.x, pos.y-1)) instanceof Porte))
+			){
+			idPorte = ((Porte)(objets.get(new Vector2f(pos.x, pos.y-1)))).getIdPorteCourante();
+		}
+		else if( // Si ce on n'est pas à gauche et qu'il y a une porte à gauche
+				(pos.x != 0)&&
+				(objets.containsKey(new Vector2f(pos.x-1, pos.y))&&
+				(objets.get(new Vector2f(pos.x-1, pos.y)) instanceof Porte))
+			){
+			idPorte = ((Porte)(objets.get(new Vector2f(pos.x-1, pos.y)))).getIdPorteCourante();
+		}
+		
+		return idPorte;
+	}
+	
+	/**
+	 * Méthode qui, couplée à la vérification de présence de porte, appelle le constructeur de Porte adéquat
+	 * @param nouvellePorte Booléan que l'on met à true si on n'a pas trouvé de bout de porte
+	 * @param idPorte Id de porte à laquelle on affecte l'id d'un bout de porte si trouvé
+	 * @param compteur Compteur de cases dans l'algorithme de lecture de fichier contenant la carte
+	 * @param objets HashMap des objets déjà trouvés sur la map
+	 * @param type Type du bout de Porte à créer
+	 * @return Objet Porte qui sera créée
+	 */
+	public static Objet assignPorte(int compteur, HashMap<Vector2f,Objet> objets, TypePorte type){
+		// Initialisation de la variable interne
+		Integer idPorte = verifPorte(compteur, objets);
+
+		/* Si on a trouvé que le bout actuel est sur une nouvelle
+		porte, on génère avec un nouvel ID */	
+		if (idPorte == -1){
+			return new Porte(type);
+		}
+		else{
+			return new Porte(type, idPorte);
 		}
 	}
 	
--- src/fr/dungeontrouble/partie/niveau/Porte.java
+++ src/fr/dungeontrouble/partie/niveau/Porte.java
@@ -7,9 +7,10 @@ import org.jsfml.graphics.Sprite;
  * Classe associée aux portes du jeu.
  * 
  * @author Maxime BELLIER
- * 
+ * @author Valentin PORCHET
  */
 public class Porte extends Objet {
+	
 	private static int compteurId = 0;
 	private int idPorteCourante = 0;
 
@@ -17,12 +18,20 @@ public class Porte extends Objet {
 		BoutPorteGauche, BoutPorteDroite, BoutPorteHaut, BoutPorteBas, MorceauPorteHorizontal, MorceauPorteVertical
 	}
 
-	public Porte(TypePorte t, boolean b) {
-		if (b) {
-			compteurId++;
-			b = false;
-		}
-		idPorteCourante = compteurId;
+	public Porte(TypePorte t) {
+		compteurId++;		
+		idPorteCourante = compteurId;		
 		this.sprite = new Sprite(texture, new IntRect(100+(50*(t.ordinal()%3)),(50*(t.ordinal()/3)), 50, 50));
+		System.out.println("Porte NOUVELLE " + idPorteCourante);
+	}
+	
+	public Porte(TypePorte t, int id){
+		idPorteCourante = id;
+		this.sprite = new Sprite(texture, new IntRect(100+(50*(t.ordinal()%3)),(50*(t.ordinal()/3)), 50, 50));
+		System.out.println("Porte SUITE " + idPorteCourante);
+	}
+
+	public int getIdPorteCourante() {
+		return idPorteCourante;
 	}
 }
