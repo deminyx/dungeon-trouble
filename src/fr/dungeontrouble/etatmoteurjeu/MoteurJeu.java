5ad295451b6dc09b8f7a41a99fac693cdaa0967b
Maxime BELLIER <maxime.bellier@etu.univ-nantes.fr>
2014-04-27 15:44:57 +0200

    Ajout de Moteur de Jeu + méthode des meilleurs scores extraite de Aff.

---
+++ src/fr/dungeontrouble/etatmoteurjeu/MoteurJeu.java
@@ -0,0 +1,69 @@
+package fr.dungeontrouble.etatmoteurjeu;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+import org.jsfml.system.Clock;
+
+import fr.dungeontrouble.evenement.Evenement;
+
+public class MoteurJeu {
+
+	private int nbJoueurs;
+	private LinkedHashMap<String, Integer> meilleursScores;
+
+	/**
+	 * Méthode de chargement des meilleurs scores à partir d'un fichier
+	 * @param path Chemin vers le fichier contenant les meilleurs scores
+	 * @return HashMap de la forme Joueur -> Score
+	 */
+	
+	public MoteurJeu(int nbJoueurs,String path) {
+		this.nbJoueurs=nbJoueurs;
+		this.meilleursScores=recupererScores("highscores.txt");
+	}
+	
+	private static LinkedHashMap<String, Integer> recupererScores(String path){
+		LinkedHashMap<String,Integer> highscores = new LinkedHashMap<String,Integer>();
+		String temp = new String(); // Chaine temporaire qui contiendra chaque ligne
+		
+		try{			
+			/* On utilise deux Scanner : un pour extraire chaque ligne
+			 * et un autre pour les analyser  
+			 */
+			
+			// Scanner extracteur de lignes
+			Scanner scannerLine = new Scanner(new File(path));
+			
+			// Scanner analyseur de lignes
+			Scanner scannerTemp = new Scanner(temp);
+			
+			while (scannerLine.hasNextLine()){
+				temp = scannerLine.nextLine();	// Récupération de la ligne	
+				scannerTemp = new Scanner(temp); // Assignation au scanner
+				highscores.put(scannerTemp.next(), scannerTemp.nextInt()); // Rajout des scores
+			}
+			
+			// On ferme les Scanner
+			scannerLine.close();
+			scannerTemp.close();
+		}
+		catch (Exception e){
+			e.printStackTrace(); // On catch l'exception en cas de probléme
+		}
+		
+		return highscores;
+	}
+
+	public void analyserEvent(Evenement e) {
+	}
+
+	public void changerPalmares() {
+	}
+
+	public void enregistrerScores() {
+	}
+
+	
+}
