26d331038eef619c0ecf089262784dc8db2c9ffc
Maxime BELLIER <maxime.bellier@etu.univ-nantes.fr>
2014-04-29 19:29:28 +0200

    Ajout d'une méthode pour la fermeture du programme

--- src/fr/dungeontrouble/evenement/QuitEvent.java
+++ src/fr/dungeontrouble/evenement/QuitEvent.java
@@ -1,6 +1,35 @@
 package fr.dungeontrouble.evenement;
 
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.event.Event;
+
+import fr.dungeontrouble.partie.entite.Personnage.TypePersonnage;
+
+/**
+ * Classe traitant l'arrêt du programme en cas de QuitEvent
+ * 
+ * @author Maxime BELLIER
+ * 
+ */
 public class QuitEvent extends Evenement {
 
-	
+	/**
+	 * Fonction de capture du clic pour arrêt du programme
+	 * 
+	 * @param window
+	 *            Fenêtre courante
+	 * @return TypePersonnage : Valeur d'enum sur le type du personnage voulu
+	 */
+	public static void getChoice(RenderWindow window) {
+		for (Event e : window.pollEvents()) {
+			switch (e.type) {
+			case CLOSED:
+				window.close();
+				System.out.println("Arrêt du programme ...");
+				System.exit(0);
+			}
+
+		}
+
+	}
 }
