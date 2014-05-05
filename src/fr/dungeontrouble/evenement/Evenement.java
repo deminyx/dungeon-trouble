5644795f9406bc080ab12fd9610009338ed06060
Maxime BELLIER <maxime.bellier@etu.univ-nantes.fr>
2014-05-05 19:07:58 +0200

    Ajout des appels aux fonctions de déplacements

--- src/fr/dungeontrouble/evenement/Evenement.java
+++ src/fr/dungeontrouble/evenement/Evenement.java
@@ -2,9 +2,10 @@ package fr.dungeontrouble.evenement;
 
 import org.jsfml.window.event.Event;
 
+import fr.dungeontrouble.partie.entite.Personnage;
+
 public abstract class Evenement {
 
 		protected Event event;
-		
 		
 }
--- src/fr/dungeontrouble/evenement/MoveEvent.java
+++ src/fr/dungeontrouble/evenement/MoveEvent.java
@@ -1,32 +1,44 @@
 package fr.dungeontrouble.evenement;
 
+import org.jsfml.system.Time;
 import org.jsfml.window.Keyboard;
 import org.jsfml.window.Keyboard.Key;
 
+import fr.dungeontrouble.partie.Partie;
+import fr.dungeontrouble.partie.entite.Entite.Direction;
+import fr.dungeontrouble.partie.entite.Personnage;
+
 public class MoveEvent extends Evenement {
 
-	public void getMove1J() {
+	
+	
+	public void getMove1J(Time t) {
 		if (Keyboard.isKeyPressed(Key.UP)) {
 			if (Keyboard.isKeyPressed(Key.RIGHT)) {
+				Partie.getP1().seDeplacer(Direction.haut_droit, t);
 			} else if (Keyboard.isKeyPressed(Key.LEFT)) {
-
+				Partie.getP1().seDeplacer(Direction.haut_gauche, t);
 			} else {
-
+				Partie.getP1().seDeplacer(Direction.haut, t);
 			}
 		} else if (Keyboard.isKeyPressed(Key.DOWN)) {
 			if (Keyboard.isKeyPressed(Key.RIGHT)) {
+				Partie.getP1().seDeplacer(Direction.bas_droit, t);
 			} else if (Keyboard.isKeyPressed(Key.LEFT)) {
+				Partie.getP1().seDeplacer(Direction.bas_gauche, t);
 
 			} else {
-
+				Partie.getP1().seDeplacer(Direction.bas, t);
 			}
 		} else if (Keyboard.isKeyPressed(Key.RIGHT)) {
+			Partie.getP1().seDeplacer(Direction.droit, t);
 		} else if (Keyboard.isKeyPressed(Key.LEFT)) {
+			Partie.getP1().seDeplacer(Direction.gauche, t);
 		}
 	}
 
-	public void getMove4J() {
-		// JOUEUR 1
+	public void getMove4J(Time t) {
+		// JOUEUR  1 : Guerrier
 		/*
 		 * Z = HAUT
 		 * D = DROITE
@@ -35,94 +47,116 @@ public class MoveEvent extends Evenement {
 		 */
 		if (Keyboard.isKeyPressed(Key.Z)) {
 			if (Keyboard.isKeyPressed(Key.D)) {
+				Partie.getP1().seDeplacer(Direction.haut_droit, t);
 			} else if (Keyboard.isKeyPressed(Key.Q)) {
-
+				Partie.getP1().seDeplacer(Direction.haut_gauche, t);
 			} else {
-
+				Partie.getP1().seDeplacer(Direction.haut, t);
 			}
 		} else if (Keyboard.isKeyPressed(Key.S)) {
 			if (Keyboard.isKeyPressed(Key.D)) {
+				Partie.getP1().seDeplacer(Direction.bas_droit, t);
 			} else if (Keyboard.isKeyPressed(Key.Q)) {
-
+				Partie.getP1().seDeplacer(Direction.bas_gauche, t);
 			} else {
-
+				Partie.getP1().seDeplacer(Direction.bas, t);
 			}
 		} else if (Keyboard.isKeyPressed(Key.D)) {
+			Partie.getP1().seDeplacer(Direction.droit, t);
 		} else if (Keyboard.isKeyPressed(Key.Q)) {
+			Partie.getP1().seDeplacer(Direction.gauche, t);
 		}
+		// JOUEUR 2 :Elfe
+				/*
+				 * NUMPAD8 = HAUT
+				 * NUMPAD6 = DROITE
+				 * NUMPAD4 = GAUCHE
+				 * NUMPAD5 = BAS
+				 */
+			if (Keyboard.isKeyPressed(Key.NUMPAD8)) {
+					if (Keyboard.isKeyPressed(Key.NUMPAD6)) {
+						Partie.getP2().seDeplacer(Direction.haut_droit, t);
+					} else if (Keyboard.isKeyPressed(Key.NUMPAD4)) {
+						Partie.getP2().seDeplacer(Direction.haut_gauche, t);
+					} else {
+						Partie.getP2().seDeplacer(Direction.haut, t);
+					}
+				} else if (Keyboard.isKeyPressed(Key.NUMPAD5)) {
+					if (Keyboard.isKeyPressed(Key.NUMPAD6)) {
+						Partie.getP2().seDeplacer(Direction.bas_droit, t);
+					} else if (Keyboard.isKeyPressed(Key.NUMPAD4)) {
+						Partie.getP2().seDeplacer(Direction.bas_gauche, t);
+					} else {
+						Partie.getP2().seDeplacer(Direction.bas, t);
+					}
+				} else if (Keyboard.isKeyPressed(Key.NUMPAD6)) {
+					Partie.getP2().seDeplacer(Direction.droit, t);
+				} else if (Keyboard.isKeyPressed(Key.NUMPAD4)) {
+					Partie.getP2().seDeplacer(Direction.gauche, t);
+				}
+		
+			// JOUEUR 3 : Magicien
+			/*
+			 * O = HAUT
+			 * M = DROITE
+			 * K = GAUCHE
+			 * L = BAS
+			 */
+			if (Keyboard.isKeyPressed(Key.O)) {
+				
+				if (Keyboard.isKeyPressed(Key.M)) {
+					Partie.getP3().seDeplacer(Direction.haut_droit, t);
+				} else if (Keyboard.isKeyPressed(Key.K)) {
+					Partie.getP3().seDeplacer(Direction.haut_gauche, t);
+				} else {
+					Partie.getP3().seDeplacer(Direction.haut, t);
+				}
+			} else if (Keyboard.isKeyPressed(Key.L)) {
+				if (Keyboard.isKeyPressed(Key.M)) {
+					Partie.getP3().seDeplacer(Direction.bas_droit, t);
+				} else if (Keyboard.isKeyPressed(Key.K)) {
+					Partie.getP3().seDeplacer(Direction.bas_gauche, t);
 
-		// JOUEUR 2
+				} else {
+					Partie.getP3().seDeplacer(Direction.bas, t);
+				}
+			} else if (Keyboard.isKeyPressed(Key.M)) {
+				Partie.getP3().seDeplacer(Direction.droit, t);
+			} else if (Keyboard.isKeyPressed(Key.K)) {
+				Partie.getP3().seDeplacer(Direction.gauche, t);
+			}
+			
+			
+			// JOUEUR 4 : Valkyrie
 		/*
 		 * T = HAUT
 		 * H = DROITE
 		 * F = GAUCHE
 		 * G = BAS
 		 */
-		else if (Keyboard.isKeyPressed(Key.T)) {
+		if (Keyboard.isKeyPressed(Key.T)) {
 			if (Keyboard.isKeyPressed(Key.H)) {
+				Partie.getP4().seDeplacer(Direction.haut_droit, t);
 			} else if (Keyboard.isKeyPressed(Key.F)) {
+				Partie.getP4().seDeplacer(Direction.haut_gauche, t);
 
 			} else {
-
+				Partie.getP4().seDeplacer(Direction.haut, t);
 			}
 		} else if (Keyboard.isKeyPressed(Key.G)) {
 			if (Keyboard.isKeyPressed(Key.H)) {
+				Partie.getP4().seDeplacer(Direction.bas_droit, t);
 			} else if (Keyboard.isKeyPressed(Key.F)) {
-
+				Partie.getP4().seDeplacer(Direction.bas_gauche, t);
 			} else {
-
+				Partie.getP4().seDeplacer(Direction.bas, t);
 			}
 		} else if (Keyboard.isKeyPressed(Key.H)) {
+			Partie.getP4().seDeplacer(Direction.droit, t);
 		} else if (Keyboard.isKeyPressed(Key.F)) {
+			Partie.getP4().seDeplacer(Direction.gauche, t);
 		}
 
-		// JOUEUR 3
-		/*
-		 * O = HAUT
-		 * M = DROITE
-		 * K = GAUCHE
-		 * L = BAS
-		 */
-		else if (Keyboard.isKeyPressed(Key.O)) {
-			if (Keyboard.isKeyPressed(Key.M)) {
-			} else if (Keyboard.isKeyPressed(Key.K)) {
-
-			} else {
-
-			}
-		} else if (Keyboard.isKeyPressed(Key.L)) {
-			if (Keyboard.isKeyPressed(Key.M)) {
-			} else if (Keyboard.isKeyPressed(Key.K)) {
-
-			} else {
-
-			}
-		} else if (Keyboard.isKeyPressed(Key.M)) {
-		} else if (Keyboard.isKeyPressed(Key.K)) {
-		}
-		// JOUEUR 4
-		/*
-		 * NUMPAD8 = HAUT
-		 * NUMPAD6 = DROITE
-		 * NUMPAD4 = GAUCHE
-		 * NUMPAD5 = BAS
-		 */
-		else if (Keyboard.isKeyPressed(Key.NUMPAD8)) {
-			if (Keyboard.isKeyPressed(Key.NUMPAD6)) {
-			} else if (Keyboard.isKeyPressed(Key.NUMPAD4)) {
-
-			} else {
-
-			}
-		} else if (Keyboard.isKeyPressed(Key.NUMPAD5)) {
-			if (Keyboard.isKeyPressed(Key.NUMPAD6)) {
-			} else if (Keyboard.isKeyPressed(Key.NUMPAD4)) {
-
-			} else {
-
-			}
-		} else if (Keyboard.isKeyPressed(Key.NUMPAD6)) {
-		} else if (Keyboard.isKeyPressed(Key.NUMPAD4)) {
-		}
+		
 	}
 }
